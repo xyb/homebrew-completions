617d238860be88bd821cd9ade89633e9f7aec32f
Pete Peterson <peterfpeterson@gmail.com>
2013-12-12 10:57:36 -0500

    add ctest bash completion
    Closes #15.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

---
+++ ctest-completion.rb
@@ -0,0 +1,11 @@
+require 'formula'
+
+class CtestCompletion < GithubGistFormula
+  url 'https://gist.github.com/peterfpeterson/7929954/raw/f63ddaf0c631bde871e4ac3384654c1ab095ea9a/ctest'
+  homepage 'https://gist.github.com/peterfpeterson/7929954'
+  sha1 '9062f6505bc53d1f5d98f7fd73fc2506f92d3efa'
+
+  def install
+    (prefix+'etc/bash_completion.d').install Dir['*']
+  end
+end
