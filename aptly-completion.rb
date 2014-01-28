f98b160c7f79b8cd1c9ae94d7d3fa0d7dfcd0adc
Andrey Smirnov <Smirnov.Andrey@gmail.com>
2014-01-28 18:36:40 +0400

    Add completion for aptly (http://www.aptly.info/).
    Closes #16.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

---
+++ aptly-completion.rb
@@ -0,0 +1,12 @@
+require 'formula'
+
+class AptlyCompletion < Formula
+  homepage 'https://github.com/aptly-dev/aptly-bash-completion'
+  url 'https://github.com/aptly-dev/aptly-bash-completion/archive/0.1.tar.gz'
+  sha1 '33b38c94a0dc998861292f29f0097d374a36b5cc'
+  head 'https://github.com/aptly/aptly-bash-completion.git'
+
+  def install
+    (prefix+'etc/bash_completion.d').install 'aptly'
+  end
+end
