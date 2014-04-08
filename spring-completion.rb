fbb0bb10218ea98d0de40e9e7217960f1e2dc533
Jason Rogers <jason.rogers@samasource.org>
2014-04-08 10:21:06 -0400

    add spring-completion.rb
    Closes #22.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

---
+++ spring-completion.rb
@@ -0,0 +1,12 @@
+require 'formula'
+
+class SpringCompletion < Formula
+  homepage 'https://github.com/jacaetevha/spring_bash_completion'
+  url 'https://github.com/jacaetevha/spring_bash_completion/archive/v0.0.1.tar.gz'
+  sha1 '13082a0054a98e08fd98bcba8d026363fa510919'
+  head 'https://github.com/jacaetevha/spring_bash_completion.git'
+
+  def install
+    (prefix+'etc/bash_completion.d').install Dir['spring.bash']
+  end
+end
