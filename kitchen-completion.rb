c3ecb8793b5d85dec1b8e2456b872355a30b744b
Colin Dean <git@cad.cx>
2014-06-26 14:53:15 -0400

    kitchen-completion v1.0.0
    For test-kitchen's `kitchen` command
    
    https://github.com/test-kitchen/test-kitchen
    
    Closes #40.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

---
+++ kitchen-completion.rb
@@ -0,0 +1,13 @@
+require 'formula'
+
+class KitchenCompletion < Formula
+  homepage "https://github.com/MarkBorcherding/test-kitchen-bash-completion"
+  url "https://github.com/MarkBorcherding/test-kitchen-bash-completion/archive/v1.0.0.tar.gz"
+  sha1 "bcafccfeb82da9b858bb75e20d0685c8a1b56e89"
+
+  head "https://raw.githubusercontent.com/MarkBorcherding/test-kitchen-bash-completion/master/kitchen-completion.bash"
+
+  def install
+    bash_completion.install "kitchen-completion.bash" => "kitchen"
+  end
+end
