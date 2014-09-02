7e0d89e95ab85bdb203580d6f4f34348867e6abf
Simon Weil <simon.weil@naturalint.com>
2014-09-02 18:01:40 +0300

    tmuxinator completion
    Closes #44.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

---
+++ tmuxinator-completion.rb
@@ -0,0 +1,13 @@
+require "formula"
+
+class TmuxinatorCompletion < Formula
+  homepage "https://github.com/tmuxinator/tmuxinator"
+  head "https://raw.githubusercontent.com/tmuxinator/tmuxinator/master/completion/tmuxinator.bash"
+  url "https://raw.githubusercontent.com/tmuxinator/tmuxinator/v0.6.8/completion/tmuxinator.bash"
+  version "0.6.8"
+  sha1 "5d3c658c31a042675c090f59174d182d87832f43"
+
+  def install
+    bash_completion.install "tmuxinator.bash" => "tmuxinator"
+  end
+end
