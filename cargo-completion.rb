d0714628c175db7142e0b1978958a8d2257064e5
Roshan George <rgeorge@liveramp.com>
2014-10-27 14:23:49 -0600

    Add completion for rustc and cargo
    Closes #46.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

---
+++ cargo-completion.rb
@@ -0,0 +1,12 @@
+require "formula"
+
+class CargoCompletion < Formula
+  homepage "https://github.com/roshan/rust-bash-completion"
+  url "https://raw.githubusercontent.com/roshan/rust-bash-completion/0.12.1/etc/bash_completion.d/cargo"
+  sha1 "24eb9bcd6553a913deca56d7ac83a06a907f7c2e"
+  head "https://raw.githubusercontent.com/roshan/rust-bash-completion/master/etc/bash_completion.d/cargo"
+
+  def install
+    bash_completion.install "cargo"
+  end
+end
---
+++ rustc-completion.rb
@@ -0,0 +1,12 @@
+require "formula"
+
+class RustcCompletion < Formula
+  homepage "https://github.com/roshan/rust-bash-completion/"
+  url "https://raw.githubusercontent.com/roshan/rust-bash-completion/0.12.1/etc/bash_completion.d/rustc"
+  sha1 "29d634bbeaba9868edca951543337014f84094ff"
+  head "https://raw.githubusercontent.com/roshan/rust-bash-completion/master/etc/bash_completion.d/rustc"
+
+  def install
+    bash_completion.install "rustc"
+  end
+end
