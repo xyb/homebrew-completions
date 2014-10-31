361e2ab73f2b8f881c4c8afd7962f81afc17e3f0
Vinta <vinta.chen@gmail.com>
2014-11-01 03:28:25 +0800

    docker-completion v1.3.1
    Closes #47.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- docker-completion.rb
+++ docker-completion.rb
@@ -1,10 +1,10 @@
 require "formula"
 
 class DockerCompletion < Formula
-  homepage "https://github.com/dotcloud/docker"
-  version "1.1.2"
-  url "https://raw.githubusercontent.com/docker/docker/v1.1.2/contrib/completion/bash/docker"
-  sha1 "451ef7ffc25f78b5228886c9f4e957b87cd50b0e"
+  homepage "https://github.com/docker/docker"
+  version "1.3.1"
+  url "https://raw.githubusercontent.com/docker/docker/v1.3.1/contrib/completion/bash/docker"
+  sha1 "33ab4d9da9f5bbe9b787699da03a1d42e25558f9"
   head "https://raw.githubusercontent.com/docker/docker/master/contrib/completion/bash/docker"
 
   def install
