de23efc74875f30e5a4dedd5bd91be1fc2f3359d
Matthias Nuessler <m.nuessler@web.de>
2014-11-08 18:07:22 +0100

    Update maven-completion to commit b22f363
    Closes #48.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- maven-completion.rb
+++ maven-completion.rb
@@ -2,9 +2,9 @@ require "formula"
 
 class MavenCompletion < Formula
   homepage "https://github.com/juven/maven-bash-completion"
-  url "https://raw.githubusercontent.com/juven/maven-bash-completion/53924da29c7b01fea3d2612b40b4b0eeacbe0af5/bash_completion.bash"
-  sha1 "e4bb4bd4638f34a11d131a299c7f1561b53f27f4"
-  version "20140719"
+  url "https://raw.githubusercontent.com/juven/maven-bash-completion/b22f363651e7a474a2e7e1fa4d7ddfaec64076c0/bash_completion.bash"
+  sha1 "e52c7cade724af565bdb4a87ea1d998fc04c489e"
+  version "20141020"
 
   def install
     bash_completion.install "bash_completion.bash" => "maven"
