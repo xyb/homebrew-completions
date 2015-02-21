735c6944d60e2492faec7bfdcf22db219ac27d77
Matthias Nuessler <m.nuessler@web.de>
2015-02-21 11:34:18 +0100

    Update maven-completion to commit 5ec4506
    Closes #50.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- maven-completion.rb
+++ maven-completion.rb
@@ -1,8 +1,8 @@
 class MavenCompletion < Formula
   homepage "https://github.com/juven/maven-bash-completion"
-  url "https://raw.githubusercontent.com/juven/maven-bash-completion/b22f363651e7a474a2e7e1fa4d7ddfaec64076c0/bash_completion.bash"
-  sha1 "e52c7cade724af565bdb4a87ea1d998fc04c489e"
-  version "20141020"
+  url "https://raw.githubusercontent.com/juven/maven-bash-completion/5ec4506b03a1e57c94f1e28c55c3b89763ec6626/bash_completion.bash"
+  sha1 "f6f601af4769869e3682284af9b6b58b68cc2590"
+  version "20150220"
 
   def install
     bash_completion.install "bash_completion.bash" => "maven"
