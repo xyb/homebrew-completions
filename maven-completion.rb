866520fe054225863f9aa5969d18d0d3406a01c6
Matthias Nuessler <m.nuessler@web.de>
2015-03-02 23:52:47 +0100

    maven-completion 1fe6ea5
    Closes #51.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- maven-completion.rb
+++ maven-completion.rb
@@ -1,8 +1,8 @@
 class MavenCompletion < Formula
   homepage "https://github.com/juven/maven-bash-completion"
-  url "https://raw.githubusercontent.com/juven/maven-bash-completion/5ec4506b03a1e57c94f1e28c55c3b89763ec6626/bash_completion.bash"
-  sha1 "f6f601af4769869e3682284af9b6b58b68cc2590"
-  version "20150220"
+  url "https://raw.githubusercontent.com/juven/maven-bash-completion/1fe6ea51ee12417a140e5c048e6079f1af7f3ebf/bash_completion.bash"
+  sha1 "2571b3e905e90cac5d8f4fbe5f12d503570bb99e"
+  version "20150302"
 
   def install
     bash_completion.install "bash_completion.bash" => "maven"
