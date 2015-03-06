ef2ed99aad325fa6fbcd2a0b07fccb75daf41d44
Yurii Kolesnykov <yurikoles@gmail.com>
2015-03-06 16:25:56 +0200

    spring-completion: fix checksum
    Closes #53.
    Closes #54.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- spring-completion.rb
+++ spring-completion.rb
@@ -1,7 +1,7 @@
 class SpringCompletion < Formula
   homepage "https://github.com/jacaetevha/spring_bash_completion"
   url "https://github.com/jacaetevha/spring_bash_completion/archive/v0.0.1.tar.gz"
-  sha1 "13082a0054a98e08fd98bcba8d026363fa510919"
+  sha1 "d6fafd061a24d0ccb549984cb081d2eea8293ec3"
   head "https://github.com/jacaetevha/spring_bash_completion.git"
 
   def install
