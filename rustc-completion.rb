2a44b492648124a4d6a1a65f585d38a6cf3fd44c
Baptiste Fontaine <batifon@yahoo.fr>
2015-08-18 00:03:02 +0200

    rustc-completion: build fixed, test added
    Closes #77.
    
    Signed-off-by: Baptiste Fontaine <batifon@yahoo.fr>

--- rustc-completion.rb
+++ rustc-completion.rb
@@ -1,10 +1,22 @@
 class RustcCompletion < Formula
-  homepage "https://github.com/roshan/rust-bash-completion/"
-  url "https://raw.githubusercontent.com/roshan/rust-bash-completion/0.12.1/etc/bash_completion.d/rustc"
-  sha256 "e758537b678a6d917ce187b8e8d65eaed9c9ab8e925d4867f312090240b159af"
-  head "https://raw.githubusercontent.com/roshan/rust-bash-completion/master/etc/bash_completion.d/rustc"
+  homepage "https://github.com/roshan/rust-bash-completion"
+  url "https://github.com/roshan/rust-bash-completion/archive/0.12.1.tar.gz"
+  sha256 "562f84ccab40f2b3e7ef47e2e6d9b6615070a0e7330d64ea5368b6ad75455012"
+
+  head "https://github.com/roshan/rust-bash-completion.git"
+
+  # upstream commit to fix an undefined command when sourcing the file directly
+  patch do
+    url "https://github.com/roshan/rust-bash-completion/commit/932e9bb4e9f28c2785de2b8db6f0e8c050f4f9be.diff"
+    sha256 "0d5f4762bd93ffe09ee86c6ffffc850337ee69a8a54049c8b29a113468b39618"
+  end
 
   def install
-    bash_completion.install "rustc"
+    bash_completion.install "etc/bash_completion.d/rustc"
+  end
+
+  test do
+    assert_match "-F _rustc",
+      shell_output("source #{bash_completion}/rustc && complete -p rustc")
   end
 end
