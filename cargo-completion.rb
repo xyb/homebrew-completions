e1b071a80eecdfe38c2fed2eb6d6b4526b9272dc
Baptiste Fontaine <batifon@yahoo.fr>
2015-08-18 00:02:25 +0200

    cargo-completion: using the official script, test added
    Closes #78.
    
    Signed-off-by: Baptiste Fontaine <batifon@yahoo.fr>

--- cargo-completion.rb
+++ cargo-completion.rb
@@ -1,10 +1,20 @@
 class CargoCompletion < Formula
   homepage "https://github.com/roshan/rust-bash-completion"
-  url "https://raw.githubusercontent.com/roshan/rust-bash-completion/0.12.1/etc/bash_completion.d/cargo"
-  sha256 "a6158c335976a05b253774babd184fa3b42c30f1165d57b3a1866c989bef76f8"
-  head "https://raw.githubusercontent.com/roshan/rust-bash-completion/master/etc/bash_completion.d/cargo"
+  url "https://github.com/rust-lang/cargo/archive/0.4.0.tar.gz"
+  sha256 "cabc2a42fa1962769a9834d0be78d401cc3684565c7d7b700e4671b8e423a044"
+  version "20150818.0.4.0"
+
+  head "https://github.com/rust-lang/cargo.git"
 
   def install
-    bash_completion.install "cargo"
+    bash_completion.install "src/etc/cargo.bashcomp.sh" => "cargo"
+    zsh_completion.install "src/etc/_cargo"
+  end
+
+  test do
+    # we need to define a dummy 'cargo' command to force the script to define
+    # the completion function
+    assert_match "-F _cargo",
+      shell_output("cargo() { true;} && source #{bash_completion}/cargo && complete -p cargo")
   end
 end
