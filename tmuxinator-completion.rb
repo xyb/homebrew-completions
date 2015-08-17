d4ce3b001e921556811de5efff8b88f7fd3c0a97
Baptiste Fontaine <batifon@yahoo.fr>
2015-08-17 14:49:33 +0200

    tmuxinator-completion 0.6.11
    Closes #72.
    
    Signed-off-by: Baptiste Fontaine <batifon@yahoo.fr>

--- tmuxinator-completion.rb
+++ tmuxinator-completion.rb
@@ -1,9 +1,9 @@
 class TmuxinatorCompletion < Formula
   homepage "https://github.com/tmuxinator/tmuxinator"
   head "https://raw.githubusercontent.com/tmuxinator/tmuxinator/master/completion/tmuxinator.bash"
-  url "https://raw.githubusercontent.com/tmuxinator/tmuxinator/v0.6.8/completion/tmuxinator.bash"
-  version "0.6.8"
-  sha256 "10d30dfa98d34c5f209f24c0931955e6909ba148743e83d9ffe3fe3ceaf9c1e2"
+  url "https://raw.githubusercontent.com/tmuxinator/tmuxinator/v0.6.11/completion/tmuxinator.bash"
+  version "0.6.11"
+  sha256 "f55d1bc4de4805eac80c71b92297b119b2c2cdcd175b290a2992ebf54fb04fba"
 
   def install
     bash_completion.install "tmuxinator.bash" => "tmuxinator"
