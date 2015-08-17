a1da8c28bb92f5d57b7e533d2efc12dbf6c93efd
Baptiste Fontaine <batifon@yahoo.fr>
2015-08-17 23:37:58 +0200

    bundler-completion: version bump; head and test added
    Closes #76.
    
    Signed-off-by: Baptiste Fontaine <batifon@yahoo.fr>

--- bundler-completion.rb
+++ bundler-completion.rb
@@ -1,10 +1,17 @@
 class BundlerCompletion < Formula
   homepage "https://github.com/mernen/completion-ruby"
-  url "https://raw.githubusercontent.com/mernen/completion-ruby/790a467456851fb563a5bbd26628465febbec2cc/completion-bundle"
-  sha256 "7fdb2ee803efb924e69d726deab1c1c5de933087d6803ac0775cd2c957df8182"
-  version "1"
+  url "https://github.com/mernen/completion-ruby.git",
+    :revision => "f3e4345042b0cc48317e45b673dfd3d23904b9a7"
+  version "2"
+
+  head "https://github.com/mernen/completion-ruby.git"
 
   def install
     bash_completion.install "completion-bundle" => "bundler"
+  end
+
+  test do
+    assert_match "-F __bundle",
+      shell_output("source #{bash_completion}/bundler && complete -p bundle")
   end
 end
