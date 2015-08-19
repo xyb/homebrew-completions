a1169287559767ff2c2cfed3bc4665d49b0930ff
Baptiste Fontaine <batifon@yahoo.fr>
2015-08-19 14:12:05 +0200

    rake-completion: test added
    Closes #90.
    
    Signed-off-by: Baptiste Fontaine <batifon@yahoo.fr>

--- rake-completion.rb
+++ rake-completion.rb
@@ -1,10 +1,15 @@
 class RakeCompletion < Formula
   homepage "https://gist.github.com/turadg/840663"
-  url "https://gist.github.com/turadg/840663/raw/46cceea5bc1f73098fcf5e099d91744b24dabdc7/rake.sh"
+  url "https://gist.githubusercontent.com/turadg/840663/raw/e58c029d656f0705b2a77632d2c3e02ff0a5e825/rake.sh"
   sha256 "262bf355a0b1f3bf134b34baeecbf6857b326218dee5ab09d0aa22302bbd271a"
   version "1"
 
   def install
     bash_completion.install "rake.sh" => "rake"
+  end
+
+  test do
+    assert_match "-F _rakecomplete",
+      shell_output("source #{bash_completion}/rake && complete -p rake")
   end
 end
