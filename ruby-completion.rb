8a7b51a8a9c38c8bab9e4686019fedbdb41243c3
Baptiste Fontaine <batifon@yahoo.fr>
2015-08-19 14:05:42 +0200

    ruby-completion: test added
    Closes #88.
    
    Signed-off-by: Baptiste Fontaine <batifon@yahoo.fr>

--- ruby-completion.rb
+++ ruby-completion.rb
@@ -1,10 +1,17 @@
 class RubyCompletion < Formula
   homepage "https://github.com/mernen/completion-ruby"
-  url "https://raw.githubusercontent.com/mernen/completion-ruby/790a467456851fb563a5bbd26628465febbec2cc/completion-ruby"
-  sha256 "a3d934fbcff7dfb29e997e58d0ceabce8de35fd4d1ff72085663c9b0eb1c1b71"
-  version "1"
+  url "https://github.com/mernen/completion-ruby.git",
+    :revision => "f3e4345042b0cc48317e45b673dfd3d23904b9a7"
+  version "2"
+
+  head "https://github.com/mernen/completion-ruby.git"
 
   def install
     bash_completion.install "completion-ruby" => "ruby"
+  end
+
+  test do
+    assert_match "-F __ruby",
+      shell_output("source #{bash_completion}/ruby && complete -p ruby")
   end
 end
