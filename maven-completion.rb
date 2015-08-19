c698d164e8a2c0e44e446d887b3f1b6d7c663711
Baptiste Fontaine <batifon@yahoo.fr>
2015-08-19 09:41:27 +0200

    maven-completion: head and test added
    Closes #84.
    
    Signed-off-by: Baptiste Fontaine <batifon@yahoo.fr>

--- maven-completion.rb
+++ maven-completion.rb
@@ -1,10 +1,17 @@
 class MavenCompletion < Formula
   homepage "https://github.com/juven/maven-bash-completion"
-  url "https://raw.githubusercontent.com/juven/maven-bash-completion/42e754fec6c29835acba8b0a5f02e483ad34201c/bash_completion.bash"
-  sha256 "ddc9ad3c090ccacde8a79b16285a62a9cfd3938a4e46208f85666caa118ab906"
+  url "https://github.com/juven/maven-bash-completion.git",
+    :revision => "42e754fec6c29835acba8b0a5f02e483ad34201c"
   version "20150510"
+
+  head "https://github.com/juven/maven-bash-completion.git"
 
   def install
     bash_completion.install "bash_completion.bash" => "maven"
+  end
+
+  test do
+    assert_match "-F _mvn",
+      shell_output("source #{bash_completion}/maven && complete -p mvn")
   end
 end
