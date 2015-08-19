91ba512284fa837819f90cad2cc313de3eea7302
Baptiste Fontaine <batifon@yahoo.fr>
2015-08-19 09:45:43 +0200

    packer-completion: head and test added
    Closes #86.
    
    Signed-off-by: Baptiste Fontaine <batifon@yahoo.fr>

--- packer-completion.rb
+++ packer-completion.rb
@@ -1,10 +1,17 @@
 class PackerCompletion < Formula
   homepage "https://github.com/mrolli/packer-bash-completion"
-  url "https://raw.githubusercontent.com/mrolli/packer-bash-completion/2bd2d9da8bdcc7a763a0c551fa9ae8617dc5ecb7/packer"
-  sha256 "f25789b4c149b6df4388fd0265ba0608a788cffc78c6aba122c526023be27e31"
+  url "https://github.com/mrolli/packer-bash-completion.git",
+    :revision => "2bd2d9da8bdcc7a763a0c551fa9ae8617dc5ecb7"
   version "0.6.0"
+
+  head "https://github.com/mrolli/packer-bash-completion.git"
 
   def install
     bash_completion.install "packer"
+  end
+
+  test do
+    assert_match "-F _packer_completion",
+      shell_output("source #{bash_completion}/packer && complete -p packer")
   end
 end
